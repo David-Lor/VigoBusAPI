c9ace37369537a8e88fbbd84d1dffa025b9b0289
David-Lor <17401854+david-lor@users.noreply.github.com>
2019-06-07 18:18:26 +0200

    Buses endpoint; ParseError on endpoints

--- vigobusapi/app.py
+++ vigobusapi/app.py
@@ -1,3 +1,5 @@
+"""Module with all the available endpoints and the FastAPI initialization
+"""
 
 # # Native # #
 from asyncio import TimeoutError
@@ -16,7 +18,8 @@ from vigobusapi.settings_handler.const import *
 
 # # Package # #
 from .vigobus_getters import html
-from .json_generator import stop_to_json
+from .vigobus_getters.exceptions import ParseError
+from .json_generator import stop_to_json, buses_to_json
 
 
 settings = load_settings()
@@ -30,6 +33,7 @@ app = FastAPI(
 
 @app.get("/stop/{stop_id}")
 async def get_stop(stop_id: int):
+    # TODO try-except-except-except... with a context manager/decorator?
     try:
         stop = await html.get_stop(stop_id)
     except TimeoutError:
@@ -38,9 +42,31 @@ async def get_stop(stop_id: int):
         raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR, detail="Generic HTTP error on external source")
     except GetterResourceUnavailable:
         raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR)
+    except ParseError:
+        raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR, detail="Error parsing external source data")
     except StopNotExist:
         stop = None
     return stop_to_json(stop)
+
+
+@app.get("/buses/{stop_id}")
+async def get_buses(stop_id: int):
+    # TODO try-except-except-except... with a context manager/decorator?
+    buses = list()
+    stop_exists = True
+    try:
+        buses = await html.get_buses(stop_id)
+    except TimeoutError:
+        raise HTTPException(status_code=HTTP_408_REQUEST_TIMEOUT, detail="Timeout on external source")
+    except HTTPError:
+        raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR, detail="Generic HTTP error on external source")
+    except GetterResourceUnavailable:
+        raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR)
+    except ParseError:
+        raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR, detail="Error parsing external source data")
+    except StopNotExist:
+        stop_exists = False
+    return buses_to_json(buses, stop_exists)
 
 
 def run():
--- vigobusapi/json_generator.py
+++ vigobusapi/json_generator.py
@@ -1,19 +1,38 @@
+"""Module to generate JSON responses for endpoint requests
+"""
 
 # # Native # #
-from typing import Optional, Dict
+from typing import Optional, Dict, List
 
 # # Installed # #
-from pybuses import Stop
+from pybuses import Stop, Bus
 
 
 def stop_to_json(stop: Optional[Stop]) -> Dict:
+    stop_exists = bool(stop)
     js = {
-        "exists": bool(stop)
+        "exists": stop_exists
     }
-    if js["exists"]:
+    if stop_exists:
         js["stopid"] = stop.stopid
         js["name"] = stop.name
         if stop.has_location():
             js["lat"] = stop.lat
             js["lon"] = stop.lon
+    return js
+
+
+def buses_to_json(buses: List[Bus], stop_exists: bool) -> Dict:
+    js = {
+        "exists": stop_exists
+    }
+    if stop_exists:
+        js["buses"] = list()
+        for bus in buses:
+            js["buses"].append({
+                "line": bus.line,
+                "route": bus.route,
+                "time": bus.time,
+                "distance": bus.distance
+            })
     return js
