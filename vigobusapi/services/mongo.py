493202e8e0e68427828df071d9ff1b1fb5a29c39
David-Lor <17401854+david-lor@users.noreply.github.com>
2021-09-13 11:24:06 +0200

    Complete docstrings for MongoDB module

--- vigobusapi/services/mongo.py
+++ vigobusapi/services/mongo.py
@@ -1,3 +1,7 @@
+"""MONGO
+MongoDB client class
+"""
+
 # # Installed # #
 from motor import motor_asyncio
 from pymongo import TEXT
@@ -8,6 +12,17 @@ from vigobusapi.logger import logger
 
 
 class MongoDB:
+    """Class containing the async MongoDB "Motor" client, and methods to acquire the database and collections used.
+    The class and Motor client are initialized and accessed in a Singleton way.
+
+    First, the current class, the Motor client and other procedures for db/collection setup are initialized using the
+    initialize() classmethod, which must be called when the API server starts.
+
+    Then, whenever the MongoDB must be accessed, the get_mongo() method can be used to fetch the MongoDB class instance
+    previously initialized, which lives as a class method.
+
+    Database settings are acquired from the "settings" module and initialized class there.
+    """
     _mongodb_instance = None  # Singleton instance of the class
     _client = motor_asyncio.AsyncIOMotorClient
 
@@ -16,6 +31,7 @@ class MongoDB:
 
     @property
     def client(self):
+        """MongoDB async "Motor" client."""
         if self._client is None:
             raise Exception("Mongo client not initialized")
         return self._client
@@ -53,7 +69,8 @@ class MongoDB:
 
     @classmethod
     def get_mongo(cls) -> "MongoDB":
-        """Singleton acquisition of MongoDB. The class should be initialized by calling the initialize() class method"""
+        """Singleton acquisition of MongoDB.
+        The class should be previously initialized by calling the initialize() class method."""
         if cls._mongodb_instance is None:
             raise Exception("Mongo class not initialized")
         return cls._mongodb_instance
