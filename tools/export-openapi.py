f51a03afda63ef61986f5c2022693b4f347810d6
David Lorenzo <17401854+David-Lor@users.noreply.github.com>
2021-09-18 14:54:46 +0200

    Export YAML OpenAPI schema

--- tools/export-openapi.py
+++ tools/export-openapi.py
@@ -1,15 +1,20 @@
 """EXPORT OPENAPI Script
 Export the OpenAPI schema of the app into a JSON file.
+Requires Python >= 3.6
 
 Usage (from cwd = repository root)
 $ python tools/export-openapi.py docs/openapi.json
-
 This will export the schema on a file inside docs/openapi.json
+
+.json and .yaml files are supported (yaml requires pyyaml, not loaded until asked).
+Multiple exporting files can be given as args, e.g.:
+$ python tools/export-openapi.py docs/openapi.json docs/openapi.yaml
+This will export the schema to two files, docs/openapi.json and docs/openapi.yaml, with JSON and YAML formats respectively
 """
 
 import os
 import sys
-import json
+from typing import List
 
 try:
     from vigobusapi import app
@@ -18,13 +23,27 @@ except ModuleNotFoundError:
     from vigobusapi import app
 
 
-def get_filename_from_args() -> str:
-    """Retrieve the output filename from called args.
-    Raise exception if no args available."""
-    file_path = sys.argv[-1]
-    if not file_path.endswith(".json"):
-        raise ValueError("Output file not given or not a .json file")
-    return file_path
+def filename_is_json(filename: str) -> bool:
+    return filename.endswith(".json")
+
+
+def filename_is_yaml(filename: str) -> bool:
+    return filename.endswith(".yaml") or filename.endswith(".yml")
+
+
+def get_filenames_from_args() -> List[str]:
+    """Retrieve the output filename/s from called args. They must be files with .json, .yaml or .yml extension.
+    Raise exception if no valid files found/given."""
+    valid_files_names = list()
+    for i, filename in enumerate(sys.argv):
+        if i == 0:
+            continue
+        if filename_is_json(filename) or filename_is_yaml(filename):
+            valid_files_names.append(filename)
+
+    if not valid_files_names:
+        raise ValueError("Output file/s not given or not .json/.yaml file/s")
+    return valid_files_names
 
 
 def get_schema() -> dict:
@@ -34,7 +53,14 @@ def get_schema() -> dict:
 
 def schema_to_json(schema: dict, indent: int = 2) -> str:
     """Convert the given JSON schema (dict) into a JSON string."""
+    import json
     return json.dumps(schema, indent=indent)
+
+
+def schema_to_yaml(schema: dict, indent: int = 2) -> str:
+    """Convert the given JSON schema (dict) into a YAML string."""
+    import yaml
+    return yaml.dump(schema, indent=indent)
 
 
 def save_to_file(filename: str, content: str):
@@ -45,10 +71,18 @@ def save_to_file(filename: str, content: str):
 
 
 def main():
-    filename = get_filename_from_args()
+    files_names = get_filename_from_args()
     schema = get_schema()
-    schema_json = schema_to_json(schema)
-    save_to_file(filename=filename, content=schema_json)
+
+    for filename in files_names:
+        if filename_is_json(filename):
+            schema_exported = schema_to_json(schema)
+        elif filename_is_yaml(filename):
+            schema_exported = schema_to_yaml(schema)
+        else:
+            continue
+
+        save_to_file(filename=filename, content=schema_exported)
 
 
 if __name__ == "__main__":
