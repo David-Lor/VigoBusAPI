2c9574ebf1fc32daee113fd4e46be84e7b56033c
David-Lor <17401854+david-lor@users.noreply.github.com>
2022-08-15 17:13:31 +0200

    chore: remove old code

--- vigobusapi/vigobus_getters/cache/__init__.py
+++
@@ -1,6 +0,0 @@
-"""CACHE
-Cache local storage for Stops and Buses
-"""
-
-from .stop_cache import *
-from .bus_cache import *
--- vigobusapi/vigobus_getters/html/__init__.py
+++
@@ -1,7 +0,0 @@
-"""HTML DATA SOURCE
-This data source come from the mobile web available on the QR codes on the Stops.
-The webpage returns up to 5 buses per page, so the Bus Getter can be requested for only the first page or all the pages.
-The Stop Getter only returns the stop name.
-"""
-
-from .html import *
